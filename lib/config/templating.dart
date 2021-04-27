fa3ff3f04483b73687ab1b223cd208442d3d5d21
Victor Nava <l16112052@cdjuarez.tecnm.mx>
2021-04-27 13:50:30 -0600

    se agrego los datos template jael

--- lib/config/templating.dart
+++ lib/config/templating.dart
@@ -1,9 +1,8 @@
 import 'package:angel_framework/angel_framework.dart';
 import 'package:angel_jael/angel_jael.dart';
 import 'package:file/file.dart';
-import 'package:code_buffer/code_buffer.dart';
+//import 'package:code_buffer/code_buffer.dart';
 
 AngelConfigurer configureServer(FileSystem fileSystem) => (Angel app) async {
-      await app.configure(jael(fileSystem.directory('views'),
-          createBuffer: () => CodeBuffer.noWhitespace()));
+      await app.configure(jael(fileSystem.directory('views')));
     };
--- lib/fz_res.dart
+++ lib/fz_res.dart
@@ -1,6 +1,12 @@
 import 'package:angel_framework/angel_framework.dart';
+import 'package:file/local.dart';
+
+import 'config/templating.dart' as templating;
 import 'routes/articles.dart' as routea;
 
 Future configureServer(Angel app) async {
+  var fs = const LocalFileSystem();
+
+  await app.configure(templating.configureServer(fs));
   await app.configure(routea.configureServer());
 }
