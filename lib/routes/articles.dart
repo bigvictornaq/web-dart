34605c230ff2e6f6f947e5185c6d10bd0ee1ba60
Victor Nava <l16112052@cdjuarez.tecnm.mx>
2021-04-27 13:38:04 -0600

    antes de fallar

---
+++ lib/routes/articles.dart
@@ -0,0 +1,7 @@
+import 'package:angel_framework/angel_framework.dart';
+
+AngelConfigurer configureServer() => (Angel app) {
+      app.get('/', (req, res) {
+        res.render('layout');
+      });
+    };
